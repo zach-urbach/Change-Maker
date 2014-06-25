ffefebebddffe185e44af53f8c28418fd2da4280
Zach Urbach <urbach-zach@gmail.com>
2014-06-25 11:00:28 -0600

    beginning of changemaker
    the start

---
+++ ChangeMaker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is ChangeMaker
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
