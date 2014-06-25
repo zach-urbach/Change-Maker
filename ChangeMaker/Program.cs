93103015ffc062cf3031b1e26bcf5e64347bd15b
Zach Urbach <urbach-zach@gmail.com>
2014-06-25 14:47:50 -0600

    more to function

--- ChangeMaker/Program.cs
+++ ChangeMaker/Program.cs
@@ -21,14 +21,34 @@ namespace ChangeMaker
         }
         static void ChangeMaker(double number)
         {
+            int QuartersTot = 0;
+            int DimesTot = 0;
+            int NickelsTot = 0;
+            int PenniesTot = 0;
             double Quarters = 0.25;
             double Dimes = 0.10;
             double Nickels = 0.05;
             double Pennies = 0.01;
 
-            if(number)
+            while (number >= 0.25 )
             {
-                
+                QuartersTot += 1;
+                number = number - 0.25;
+            }
+            while (number >= 0.10)
+            {
+                DimesTot += 1;
+                number = number - 0.10;
+            }
+            while (number >= 0.05)
+            {
+                NickelsTot += 1;
+                number = number - 0.05;
+            }
+            while (number >= 0.01)
+            {
+                PenniesTot += 1;
+                number = number - 0.01;
             }
 
 
